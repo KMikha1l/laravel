c9a9cbc308d5da360d67d8ddd850f23f09ef14b8
Mikhail Kobyshev <333kmihail333@gmail.com>
2018-11-08 17:48:37 +0300

    "duplicate model Post deleted"

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -1,14 +1,19 @@
 <?php
-namespace App;
+namespace App\Models;
 
 use \Illuminate\Database\Eloquent\Model;
+use \Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Post extends Model
 {
-  protected $guarded = [];
+    protected $fillable = [
+      'user_id',
+      'title',
+      'content',
+    ];
 
-  public function owner()
-  {
-    return $this->hasOne('App\User');
-  }
+    public function owner(): HasOne
+    {
+        return $this->hasOne('App\User', 'id', 'user_id');
+    }
 }
