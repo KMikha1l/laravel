903eb7e830918e10076f24c774026f524c6ea2b5
Mikhail Kobyshev <333kmihail333@gmail.com>
2018-12-14 16:21:07 +0300

    config fix

--- tests/Unit/App/Models/UserModelTest.php
+++ tests/Unit/App/Models/UserModelTest.php
@@ -82,6 +82,7 @@ class UserModelTest extends TestCase
      */
     public function initialData()
     {
+        Artisan::call('config:cache');
         Artisan::call('db:seed');
 
         $data = [
